31ae450cfaf99e26151c61e4707d28829e82b6bc
osaxma <46427323+osaxma@users.noreply.github.com>
2021-12-21 15:16:59 +0300

    modified trace

--- lib/src/find_unused_packages/find_unused_packages.dart
+++ lib/src/find_unused_packages/find_unused_packages.dart
@@ -32,14 +32,17 @@ class UnusedPackagesFinder {
     await getAllPackagesInDartFilesFromLibFolder(directory);
 
     logger.trace('found the following dart files:');
-    dartFilesInLibFolder.forEach((f) => logger.trace(p.relative(f.path)));
+    dartFilesInLibFolder.forEach((f) => logger.trace(p.relative(f.path, from: directory.path)));
     logger.trace('found the following dependencies:');
     logger.trace(dependencies.reduce((value, element) => value + ' | ' + element));
 
-    final importedPackages = CollectImportsVisitor.getAllPackages(dartFilesInLibFolder, logger);
+    final importedPackages = _CollectImportsVisitor.getAllPackages(dartFilesInLibFolder, logger);
     logger.trace('the following are the imported packages');
     importedPackages.forEach(logger.trace);
+    logger.trace(importedPackages.reduce((value, element) => value + ' | ' + element));
+
     final unusedPackages = _findUnusedPackages(importedPackages);
+    logger.trace('the following are the unused packages');
     unusedPackages.forEach((val) => logger.trace(' - $val'));
     return unusedPackages;
   }
@@ -62,36 +65,38 @@ class UnusedPackagesFinder {
   }
 
   Future<void> getAllPackagesInDartFilesFromLibFolder(Directory directory) async {
-    final dartFiles = await getAllDartFilesFromLibDirectory(directory);
+    final dartFiles = await findAllDartFiles(directory);
     dartFilesInLibFolder.addAll(dartFiles);
   }
 }
 
-class CollectImportsVisitor extends RecursiveAstVisitor {
+class _CollectImportsVisitor extends RecursiveAstVisitor {
   final imports = <String>[];
   final Logger logger;
-  CollectImportsVisitor(this.logger);
+  _CollectImportsVisitor(this.logger);
 
   static List<String> getAllPackages(List<File> files, Logger logger) {
     // TODO: look how build_runner does it (trace thru Freezed or GQL)
     //       does it read file by file, or load all files?
-    final visitor = CollectImportsVisitor(logger);
+    final visitor = _CollectImportsVisitor(logger);
     for (var file in files) {
-      logger.trace('reading ${p.relative(file.path)}');
+      logger.trace('--> reading ${p.basename(file.path)}');
       final ast = generateASTfromFile(file);
+      // TODO: change to visit children and I think we won't need `CollectImportsVisitor` to be `RecursiveAstVisitor`
       ast.accept(visitor);
     }
-    return visitor.imports;
+    return visitor.imports.toSet().toList();
   }
 
   @override
   visitImportDirective(ImportDirective node) {
     final uri = node.uri.stringValue;
+    print('IMPORT URI = ${node.uri.stringValue}');
     if (uri != null) {
       final packageName = extractPackageNameFromImportUri(uri);
       if (packageName != null) {
         imports.add(packageName);
-        logger.trace('found import for package name: $packageName');
+        logger.trace('------> found: $packageName package');
       }
     }
     return super.visitImportDirective(node);
